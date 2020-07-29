c46b702829ea337fc111c4c878c11b2eecfaf770
NewCoderAS <68166034+NewCoderAS@users.noreply.github.com>
2020-07-29 22:06:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.css"
+const api={
+  key:"4aa52bb932cb6c66d1564450ba93380e",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
+function App() {
+const[query, setQuery]=useState('');
+const[weather, setWeather]=useState({});
+
+const search = evt =>{
+  if(evt.key==="Enter"){
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(res=>res.json())
+    .then(result=> {
+      setWeather(result);
+      setQuery('');
+      console.log(result);
+    });
+  }
+}
+  const dateBuilder =(d) =>{
+    let months=["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+    
+    let day=days[d.getDay()];
+    let date=d.getDate();
+    let month=months[d.getMonth()];
+    let year=d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  
+  }
+  return (
+    <div className={(typeof weather.main!="undefined") ? (weather.main.temp > 16) ? "app warm" : "app" : "app warm"}>
+      <main>
+      <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."
+          onChange={e=> setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+          />        
+      </div>
+        {(typeof weather.main!="undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name},{weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+           <div className="temp">
+              {Math.round(weather.main.temp)}&#176;C
+           </div>
+           <div className="weather">{weather.weather[0].main}</div><br/>
+           <div className="feel">Feels Like <span className="feelbox">{Math.round(weather.main.feels_like)}&#176;C</span></div><br/>
+           <div className="minmax ">Min. Temp <span className="feelbox">{Math.round(weather.main.temp_min)}&#176;C</span></div>
+           <div className="minmax">Max. Temp <span className="feelbox">{Math.round(weather.main.temp_max)}&#176;C</span></div>
+          </div>
+        </div>) : ("")}
+      </main>
+    </div>
+  );
+}
+
+export default App;
