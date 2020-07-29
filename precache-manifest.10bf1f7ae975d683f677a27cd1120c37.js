7b4d96d4dce194a19fb0ccdc36dc95f2306cedcd
Aryak Sathye <aryak.sathye@gmail.com>
2020-07-29 20:26:04 +0530

    Updates

---
+++ precache-manifest.10bf1f7ae975d683f677a27cd1120c37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de50d418df0308a73944d7600500cd1a",
+    "url": "/%7BWeather%7D/index.html"
+  },
+  {
+    "revision": "b8eade22dff01b40d5b9",
+    "url": "/%7BWeather%7D/static/css/main.72d7980c.chunk.css"
+  },
+  {
+    "revision": "de1e6d83cbeecba7f4c9",
+    "url": "/%7BWeather%7D/static/js/2.1bbc599e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BWeather%7D/static/js/2.1bbc599e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8eade22dff01b40d5b9",
+    "url": "/%7BWeather%7D/static/js/main.91ed62c6.chunk.js"
+  },
+  {
+    "revision": "2bd10273db041617ebc4",
+    "url": "/%7BWeather%7D/static/js/runtime-main.273a90af.js"
+  },
+  {
+    "revision": "8cd260d383ea6b65af974cbce41d0b59",
+    "url": "/%7BWeather%7D/static/media/cold-fbg.8cd260d3.jpg"
+  },
+  {
+    "revision": "1f002098cbd3f353cd25979ef7208df2",
+    "url": "/%7BWeather%7D/static/media/warm-bg.1f002098.jpg"
+  }
+]);
